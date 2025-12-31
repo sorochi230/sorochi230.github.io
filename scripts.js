73770df1d49b531cdde8f73c3760326c0641afde
sorochi230 <ugean13@gmail.com>
2025-12-31 01:11:53 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const learnBtn = document.getElementById("learnBtn");
+
+    learnBtn.addEventListener("click", function () {
+        alert(
+            "Chukwu Sorochi Nkemka\n" +
+            "Software Engineering Student\n" +
+            "Bowen University"
+        );
+    });
+});
